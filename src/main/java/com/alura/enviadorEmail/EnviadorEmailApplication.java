21708dd11c36594f2d0ac5c63b4bd94359424dcf
Rafael Vasconcellos <rafael.vasconcellos@accenture.com>
2019-05-03 11:39:01 -0300

    Criando o projeto

---
+++ src/main/java/com/alura/enviadorEmail/EnviadorEmailApplication.java
@@ -0,0 +1,13 @@
+package com.alura.enviadorEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviadorEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviadorEmailApplication.class, args);
+	}
+
+}
